ea00dbb4d260957b4510d8ee37d80e837684b5f9
Markus <karl.markus.larsson@gmail.com>
2017-03-30 18:59:31 +0200

    initial android commit, added map

---
+++ android/app/src/main/java/phone/my/whereismyphone/activities/MapsActivity.java
@@ -0,0 +1,42 @@
+package phone.my.whereismyphone.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import phone.my.whereismyphone.R;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        initViews();
+        setupMap();
+    }
+
+    private void setupMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                onMapReady(googleMap);
+            }
+        });
+    }
+
+    private void initViews() {
+
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+}
