aa841da817f393c986de0430e65b64282c67623f
Michael Rybacki <michael.rybacki@follow-up.com>
2017-03-30 19:06:34 +0200

    Spring: Added pom & first controller

---
+++ spring/src/main/java/com/letnight/whereismyphone/InputController.java
@@ -0,0 +1,14 @@
+package com.letnight.whereismyphone;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InputController {
+
+    @PostMapping("/position")
+    public void inputPosition() {
+
+    }
+
+}
