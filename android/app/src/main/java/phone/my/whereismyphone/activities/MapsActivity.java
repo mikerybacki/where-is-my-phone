0120296948ff3230f237769bd8b030569f1d99b8
Markus <karl.markus.larsson@gmail.com>
2017-03-30 20:51:26 +0200

    added refresh button

--- android/app/src/main/java/phone/my/whereismyphone/activities/MapsActivity.java
+++ android/app/src/main/java/phone/my/whereismyphone/activities/MapsActivity.java
@@ -1,13 +1,20 @@
 package phone.my.whereismyphone.activities;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import phone.my.whereismyphone.Cache;
 import phone.my.whereismyphone.CustomApplication;
 import phone.my.whereismyphone.R;
 import phone.my.whereismyphone.helpers.LocationHelper;
@@ -16,14 +23,48 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
     private LocationHelper mLocationHelper;
+    private View mFab;
+    private Marker mCurrentPlaceMarker;
+    private boolean mFirstMarker = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         initViews();
+        setupCallbacks();
         setupHelpers();
         setupMap();
+    }
+
+    private void setupCallbacks() {
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePositions();
+            }
+        });
+    }
+
+    private void updatePositions() {
+        if (mMap != null) {
+            Location location = Cache.sharedInstance().getLocation();
+            if (location != null) {
+                LatLng mCurrentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                if (mCurrentPlaceMarker == null) {
+                    MarkerOptions marker = new MarkerOptions().position(
+                            mCurrentLatLng);
+                    mCurrentPlaceMarker = mMap.addMarker(marker);
+                } else {
+                    mCurrentPlaceMarker.setPosition(mCurrentLatLng);
+                }
+                if (mFirstMarker) {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(mCurrentLatLng));
+                    //mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+                    mFirstMarker = false;
+                }
+            }
+        }
     }
 
     private void setupHelpers() {
@@ -52,11 +93,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void initViews() {
+        mFab = findViewById(R.id.fab);
 
     }
 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        updatePositions();
     }
 
     @Override
